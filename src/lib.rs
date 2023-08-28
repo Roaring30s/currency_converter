40c8e4853c4bef6e2d06f5b4a7fad02608c2bd97
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-08-28 08:03:18 -0400

    :hammer: Build Exchange Rate Converter

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,44 @@
+use reqwest;
 use std::env;
 use dotenv::dotenv;
+use serde_json::{Value};
 
-pub const CURRENCIES: [&str; 2] = ["COP", "EUR"];
+pub const CURRENCIES: [&str; 3] = ["COP", "EUR", "AED"];
+const FOREX_ENDPOINT: &str = "http://data.fixer.io/api/latest?access_key=";
 
-pub fn obtain_forex_key() -> String {
+fn obtain_forex_key() -> String {
     dotenv().ok(); //loads .env info into rust environment
     let key = env::var("FOREX_KEY").expect("Issue with Forex Env Var");
     
     key
+}
+
+pub fn calculate_exchange_rate(currency: &String) -> String {
+    
+    let fx_key = obtain_forex_key();
+    let error = "There was an error calculating the exchange rate.".to_string();
+    let url = FOREX_ENDPOINT.to_string()+&fx_key;
+
+    // API Request
+    let resp = reqwest::blocking::get(url).unwrap();
+    
+    // Parse
+    if resp.status().is_success() {
+        let stringified_json = resp.text().unwrap();
+        let str_reference: &str = &stringified_json;
+        let parsed_json: Value = serde_json::from_str(str_reference).unwrap();
+        //refactor me later
+        if let Some(rates) = parsed_json.get("rates") {
+            if let Some(rate) = rates.get(currency) {
+                let captured_rate = rate.clone();
+                captured_rate.to_string()
+            } else {
+                error
+            }
+        } else {
+            error
+        }
+    } else {
+        error
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,16 @@ fn main() {
         panic!("Only one currency please");
     }
 
-    let selected_currency = args[1].to_uppercase();
-    let forex_key = lib::obtain_forex_key(); 
-    println!("{}", forex_key);
+    let selected_currency = args[1].to_uppercase(); 
 
+    if !lib::CURRENCIES.iter().any(|&currency| currency == selected_currency) {
+        println!("Pick another supported currency or cargo run --help");
+    }
+
+    let rate = lib::calculate_exchange_rate(&selected_currency);
+    println!("1 Euro is {} {}", rate, selected_currency);
+    //let test = lib::calculateExchangeRate();
+    //dbg!(test);
     //conduct api call using lib code here. 
     
 }
