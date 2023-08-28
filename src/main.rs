0dc89d5aad86eb39099fa9fd57d04c5af36298fb
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-08-28 08:06:50 -0400

    :wrench: Fix Comments

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,5 @@ fn main() {
 
     let rate = lib::calculate_exchange_rate(&selected_currency);
     println!("1 Euro is {} {}", rate, selected_currency);
-    //let test = lib::calculateExchangeRate();
-    //dbg!(test);
-    //conduct api call using lib code here. 
-    
-}
 
-/*
- * take in the cmd argument
- * verify if empty or valid supported currency
- * call to free api endpoint to fetch current exchange rate 
- * return exchange rate to the cmd prompt
- * Set a help flag that will return the supported currencies. 
-*/
+}
