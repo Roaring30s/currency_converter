94b26e1c4d75366e83983f7cf1f8104921057253
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-08-28 03:44:28 -0400

    :hammer: Initiate Project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+ * take in the cmd argument
+ * verify if empty or valid supported currency
+ * call to free api endpoint to fetch current exchange rate 
+ * return exchange rate to the cmd prompt
+*/
