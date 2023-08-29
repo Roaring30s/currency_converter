3b0fc1d4bd23127f3dd9d96590970100c23ae95f
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-08-29 07:44:35 -0400

    :wrench: Refactor Lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
 use reqwest;
 use std::env;
 use dotenv::dotenv;
-use serde_json::{Value};
+use serde::Deserialize;
+
+/*
+ * derive - forces traits into your struct
+ * Debug - allows you to print {:?} struct for debugging purposes
+ * Deserialize - grabs this trait from serde that will then map the json keys to your struct keys
+ *    a. the opposite is serialization which converts easy-to-read data into JSON, XML or binary data
+*/
+#[derive(Debug, Deserialize)]
+struct RatesBook {
+    success: bool,
+    timestamp: u64,
+    base: String,
+    date: String,
+    rates: std::collections::HashMap<String, f64>,
+}
 
 pub const CURRENCIES: [&str; 3] = ["COP", "USD", "AED"];
 const FOREX_ENDPOINT: &str = "http://data.fixer.io/api/latest?access_key=";
@@ -24,16 +39,17 @@ pub fn calculate_exchange_rate(currency: &String) -> String {
     
     // Parse
     if resp.status().is_success() {
-        let stringified_json = resp.text().unwrap();
-        let str_reference: &str = &stringified_json;
-        let parsed_json: Value = serde_json::from_str(str_reference).unwrap();
-        //refactor me later
-        if let Some(rates) = parsed_json.get("rates") {
-            if let Some(rate) = rates.get(currency) {
-                let captured_rate = rate.clone();
-                captured_rate.to_string()
-            } else {
-                error
+        /*
+         * To use Json
+         * 1. Have a struct declaring what the returned JSON will look like & deserialize it
+         *    a. Deserialize? Grabs messy data json and organizes into Rust types i.e. structs
+         * 2 . Include the json feature in reqwest with the blocking in Cargo.toml
+         */
+        //response data being deserialized to json in .json, then deserialized to your struct
+        if let Ok(parsed_json) = resp.json::<RatesBook>() { //telling reqwest library to convert the JSON to RatesBook type using the deserialization method provided by serde json
+            match parsed_json.rates.get(currency) {
+                Some(rate) => rate.to_string(),
+                None => error,
             }
         } else {
             error
@@ -42,3 +58,14 @@ pub fn calculate_exchange_rate(currency: &String) -> String {
         error
     }
 }
+
+
+
+
+/*
+
+2. Implement help flag
+
+3. What is a better way than unwrap
+
+*/
